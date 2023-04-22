b7f1747134c3943816c60f164534500f691c4144
LauraCamT <130876406+LauraCamT@users.noreply.github.com>
2023-04-22 17:16:27 -0500

    Update AgendaAmigos.java

--- src/co/edu/unbosque/model/AgendaAmigos.java
+++ src/co/edu/unbosque/model/AgendaAmigos.java
@@ -3,24 +3,44 @@ package co.edu.unbosque.model;
 import java.util.ArrayList;
 import java.io.Serializable;
 
+ /**
+     * Clase AgendaAmigos implementa la interfaz "Serializable". Los objetos de esta clase pueden ser serializados o deserializados.
+     */
 
-public class AgendaAmigos implements Serializable {
+   public class AgendaAmigos implements Serializable {
 
     private ArrayList<ContactoAmigo> contactosAmigos;
+/**
+     * Constructor que inicializa la lista de contactos de amigos vacia.
+     */
 
     public AgendaAmigos() {
         contactosAmigos = new ArrayList<ContactoAmigo>();
     }
-
+       
+ /**
+     * Método que devuelve la lista de contacto de amigos almacenado en la agenda.
+     * @return la lista de contactos de amigos almacenados en la agenda.
+     */
+       
     public ArrayList<ContactoAmigo> getContactosAmigos() {
         return contactosAmigos;
     }
-
+    
+     /**
+     * Método que establece la lista de contacto de amigos almacenados en la agenda.
+     * @param contactosAmigos la nueva lista de contactos de amigos a almacenar en la agenda.
+     */
+       
     public void setContactosAmigos(ArrayList<ContactoAmigo> contactosAmigos) {
         this.contactosAmigos = contactosAmigos;
     }
     
-
+ /**
+     * Método toString devuelve una cadena de texto que representa la agenda de amigos.
+     * @return Cadena de texto que representa la agenda de amigos.
+     */
+       
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
