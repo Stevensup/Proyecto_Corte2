cb348dc74a6ff35066004e43a944440fc830a7e1
Brayan Steven Useche Palomino <buseche@unbosque.edu.co>
2023-04-22 22:34:36 -0500

    diagrama clases

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -2,6 +2,7 @@ package co.edu.unbosque.controller;
 
 import co.edu.unbosque.model.ContactoAmigo;
 import co.edu.unbosque.model.ContactoTrabajo;
+import co.edu.unbosque.view.VistaConsola;
 
 import java.util.Scanner;
 // import co.edu.unbosque.model.ContactoTrabajo;
@@ -11,12 +12,14 @@ public class Controller {
     private Scanner scanner;
     private ContactoAmigo Camigo;
     private ContactoTrabajo Ctrabajo;
+    private VistaConsola vista;
 /**
       * Constructor de la clase Controller, inicializa los modelos a utilizar.
       */
 	
     public Controller() {
         scanner = new Scanner(System.in);
+        vista = new VistaConsola();
         Camigo = new ContactoAmigo(null, null, null, null);
         Ctrabajo = new ContactoTrabajo(null, null, null, null, null);
        
@@ -28,13 +31,13 @@ public class Controller {
     public void start() {
         int option = 0;
         do {
-            System.out.println("-------------------Escoge una opcion:");
-            System.out.println("-------------------1. Ingresar Agenda Contactos Amigos: ");
-            System.out.println("-------------------2. Ingresar Agenda Contactos Trabajo");
-            System.out.println("-------------------3. Buscar y editar contacto amigo");
-            System.out.println("-------------------4. Listar contactos");
-            System.out.println("-------------------5. Eliminar contacto");
-            System.out.println("-------------------6. Salir");
+            vista.mostrarInformacion("-------------------Escoge una opcion:");
+            vista.mostrarInformacion("-------------------1. Ingresar Agenda Contactos Amigos: ");
+            vista.mostrarInformacion("-------------------2. Ingresar Agenda Contactos Trabajo");
+            vista.mostrarInformacion("-------------------3. Buscar y editar contacto amigo");
+            vista.mostrarInformacion("-------------------4. Listar contactos");
+            vista.mostrarInformacion("-------------------5. Eliminar contacto");
+            vista.mostrarInformacion("-------------------6. Salir");
             System.out.print("-------------------Ingrese la opcion: ");
             option = scanner.nextInt();
             scanner.nextLine(); // consumir el salto de línea pendiente
@@ -44,13 +47,13 @@ public class Controller {
 	 */
             switch (option) {
                 case 1:
-                    System.out.println("Ingrese los datos del contacto amigo: ");
+                    vista.mostrarInformacion("Ingrese los datos del contacto amigo: ");
     
                     System.out.print("Nombre: ");
                     String nombreAmigo = scanner.nextLine();
                     // Verificar si el nombre contiene caracteres no permitidos
                     while (nombreAmigo.matches(".*\\d.*") || nombreAmigo.matches(".*[^a-zA-Z\\s].*")) {
-                        System.out.println("El nombre solo puede contener letras y espacios en blanco. Intente de nuevo.");
+                        vista.mostrarInformacion("El nombre solo puede contener letras y espacios en blanco. Intente de nuevo.");
                         nombreAmigo = scanner.nextLine();
                     }
                     // Verificar si el telefono contiene letras o caracteres no permitidos
@@ -62,22 +65,22 @@ public class Controller {
                         if (numeroseparado.matches("[0-9]+")) {
                             esNumerico = true;
                         } else {
-                            System.out.println("Por favor ingrese solo numeros.");
+                            vista.mostrarInformacion("Por favor ingrese solo numeros.");
                             System.out.print("Telefono: ");
                         }
                     }
                     String telefonoAmigo = numeroseparado.substring(0, 3) + "-" + numeroseparado.substring(3, 6) + "-" + numeroseparado.substring(6,9);
-                    System.out.println(telefonoAmigo);
+                    vista.mostrarInformacion(telefonoAmigo);
     
                     //verificar que el correo cuente con el @
                     System.out.print("Correo: ");
                     String correoAmigo = scanner.nextLine();
                     while (!correoAmigo.contains("@")) {
-                        System.out.println("El correo electrónico debe contener el símbolo '@'. Por favor, ingrese un correo válido:");
+                        vista.mostrarInformacion("El correo electrónico debe contener el símbolo '@'. Por favor, ingrese un correo válido:");
                         correoAmigo = scanner.nextLine();
                     }
 
-                    System.out.println("Países permitidos: " + Camigo.paises);
+                    vista.mostrarInformacion("Países permitidos: " + Camigo.paises);
                     String paisAmigo = null;
                     while (paisAmigo == null) {
                         System.out.print("Pais: ");
@@ -85,7 +88,7 @@ public class Controller {
                         if (Camigo.paises.contains(input)) {
                             paisAmigo = input;
                         } else {
-                            System.out.println("Pais no valido. Por favor ingrese un pais valido.");
+                            vista.mostrarInformacion("Pais no valido. Por favor ingrese un pais valido.");
                         }
                     }
 
@@ -99,13 +102,13 @@ public class Controller {
 	 */
 			    
                 case 2:    
-                    System.out.println("Ingrese los datos del contacto de trabajo: ");
+                    vista.mostrarInformacion("Ingrese los datos del contacto de trabajo: ");
                 
                     System.out.print("Nombre: ");
                     String nombreTrabajo = scanner.nextLine();
                     // Verificar si el nombre contiene caracteres no permitidos
                     while (nombreTrabajo.matches(".*\\d.*") || nombreTrabajo.matches(".*[^a-zA-Z\\s].*")) {
-                        System.out.println("El nombre solo puede contener letras y espacios en blanco. Intente de nuevo.");
+                        vista.mostrarInformacion("El nombre solo puede contener letras y espacios en blanco. Intente de nuevo.");
                         nombreTrabajo = scanner.nextLine();
                     }
                     
@@ -113,7 +116,7 @@ public class Controller {
                     String nombreEmpresa = scanner.nextLine();
                     // Verificar si el nombre contiene caracteres no permitidos
                     while (nombreEmpresa.matches(".*\\d.*") || nombreEmpresa.matches(".*[^a-zA-Z\\s].*")) {
-                        System.out.println("El nombre solo puede contener letras y espacios en blanco. Intente de nuevo.");
+                        vista.mostrarInformacion("El nombre solo puede contener letras y espacios en blanco. Intente de nuevo.");
                         nombreEmpresa = scanner.nextLine();
                     }
                     // Verificar si el telefono contiene letras o caracteres no permitidos
@@ -125,22 +128,22 @@ public class Controller {
                         if (numeroseparadoTrabajo.matches("[0-9]+")) {
                             esNumericoTrabajo = true;
                         } else {
-                            System.out.println("Por favor ingrese solo numeros.");
+                            vista.mostrarInformacion("Por favor ingrese solo numeros.");
                             System.out.print("Telefono: ");
                         }
                     }
                     String telefonoTrabajo = numeroseparadoTrabajo.substring(0, 3) + "-" + numeroseparadoTrabajo.substring(3, 6) + "-" + numeroseparadoTrabajo.substring(6,9);
-                    System.out.println(telefonoTrabajo);
+                    vista.mostrarInformacion(telefonoTrabajo);
                 
                     //verificar que el correo cuente con el @
                     System.out.print("Correo: ");
                     String correoTrabajo = scanner.nextLine();
                     while (!correoTrabajo.contains("@")) {
-                        System.out.println("El correo electrónico debe contener el símbolo '@'. Por favor, ingrese un correo válido:");
+                        vista.mostrarInformacion("El correo electrónico debe contener el símbolo '@'. Por favor, ingrese un correo válido:");
                         correoTrabajo = scanner.nextLine();
                     }
                 
-                    System.out.println("Países permitidos: " + Ctrabajo.paises);
+                    vista.mostrarInformacion("Países permitidos: " + Ctrabajo.paises);
                     String paisTrabajo = null;
                     while (paisTrabajo == null) {
                         System.out.print("Pais: ");
@@ -148,7 +151,7 @@ public class Controller {
                         if (Ctrabajo.paises.contains(input)) {
                             paisTrabajo = input;
                         } else {
-                            System.out.println("Pais no valido. Por favor ingrese un pais valido.");
+                            vista.mostrarInformacion("Pais no valido. Por favor ingrese un pais valido.");
                         }
                     }
                 
@@ -166,10 +169,10 @@ public class Controller {
   		 // Buscar el contacto amigo en la AgendaAmigos
                     ContactoAmigo contacto = Camigo.buscarContacto(nombreBuscar);
                     if (contacto == null) {
-                        System.out.println("No se encontró ningún contacto amigo con ese nombre.");
+                        vista.mostrarInformacion("No se encontró ningún contacto amigo con ese nombre.");
                     } else {
-                        System.out.println("Contacto encontrado: " + contacto.toString());
-                        System.out.println("Ingrese los nuevos datos del contacto amigo:");
+                        vista.mostrarInformacion("Contacto encontrado: " + contacto.toString());
+                        vista.mostrarInformacion("Ingrese los nuevos datos del contacto amigo:");
     
                         System.out.print("Nombre: ");
                         String nuevoNombre = scanner.nextLine();
@@ -184,7 +187,7 @@ public class Controller {
                         String nuevoPais = scanner.nextLine();
     
                         Camigo.editarContacto(nuevoNombre, nuevoTelefono, nuevoCorreo, nuevoPais);
-                        System.out.println("Contacto amigo editado exitosamente.");
+                        vista.mostrarInformacion("Contacto amigo editado exitosamente.");
                     }
                     break;
 /**
@@ -196,16 +199,16 @@ public class Controller {
 	 */	 
 			    
                 case 4:
-                        System.out.println("Lista de contactos: ");
+                        vista.mostrarInformacion("Lista de contactos: ");
                         Camigo.listarContactos();
-                        System.out.println("------------------------------");
-                        System.out.println("Lista de contactos trabajo : ");
+                        vista.mostrarInformacion("------------------------------");
+                        vista.mostrarInformacion("Lista de contactos trabajo : ");
                         Ctrabajo.listarContactosTrabajo();
                     break;
                     case 5:
-                        System.out.println("¿Qué tipo de contacto desea eliminar?");
-                        System.out.println("1. Contacto Amigo");
-                        System.out.println("2. Contacto Trabajo");
+                        vista.mostrarInformacion("¿Qué tipo de contacto desea eliminar?");
+                        vista.mostrarInformacion("1. Contacto Amigo");
+                        vista.mostrarInformacion("2. Contacto Trabajo");
                         int opcion = scanner.nextInt();
                         scanner.nextLine(); // consume the newline character
                     switch (opcion) {
@@ -214,9 +217,9 @@ public class Controller {
                             String nombre = scanner.nextLine();
                             boolean eliminadoAmigo = Camigo.eliminarContacto(nombre);
                             if (eliminadoAmigo) {
-                                System.out.println("El contacto " + nombre + " ha sido eliminado correctamente.");
+                                vista.mostrarInformacion("El contacto " + nombre + " ha sido eliminado correctamente.");
                             } else {
-                                System.out.println("No se pudo eliminar el contacto " + nombre + ". Verifique que el nombre sea correcto.");
+                                vista.mostrarInformacion("No se pudo eliminar el contacto " + nombre + ". Verifique que el nombre sea correcto.");
                             }
                             break;
                         case 2:
@@ -224,21 +227,21 @@ public class Controller {
                             String nombretrabajo = scanner.nextLine();
                             boolean eliminadoTrabajo = Ctrabajo.eliminarContactoTrabajo(nombretrabajo);
                             if (eliminadoTrabajo) {
-                                System.out.println("El contacto trabajo (empresa) " + nombretrabajo + " ha sido eliminado correctamente.");
+                                vista.mostrarInformacion("El contacto trabajo (empresa) " + nombretrabajo + " ha sido eliminado correctamente.");
                             } else {
-                                System.out.println("No se pudo eliminar el contacto trabajo " + nombretrabajo + ". Verifique que el nombre sea correcto.");
+                                vista.mostrarInformacion("No se pudo eliminar el contacto trabajo " + nombretrabajo + ". Verifique que el nombre sea correcto.");
                             }
                             break;
                         default:
-                            System.out.println("Opción inválida.");
+                            vista.mostrarInformacion("Opción inválida.");
                             break;
                     }
                     break;
                 case 6:
-                    System.out.println("Hasta pronto...");
+                    vista.mostrarInformacion("Hasta pronto...");
                     break;
                 default:
-                    System.out.println("Opcion invalida...");
+                    vista.mostrarInformacion("Opcion invalida...");
                     break;
             }
     
