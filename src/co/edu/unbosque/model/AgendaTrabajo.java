909ae4edb5c24e1449dc092d131442833c9954ee
LauraCamT <130876406+LauraCamT@users.noreply.github.com>
2023-04-22 21:01:07 -0500

    Update AgendaTrabajo.java

--- src/co/edu/unbosque/model/AgendaTrabajo.java
+++ src/co/edu/unbosque/model/AgendaTrabajo.java
@@ -3,24 +3,41 @@ package co.edu.unbosque.model;
 import java.util.ArrayList;
 import java.io.Serializable;
 
+/**
+     * Clase AgendaTrabajo implementa la interfaz "Serializable". Los objetos de esta clase pueden ser serializados o deserializados.
+     */
 
 public class AgendaTrabajo implements Serializable {
 
     private ArrayList<ContactoTrabajo> contactosTrabajo;
+    
+    /**
+     * Constructor que inicializa la lista de contactos de trabajo vacia.
+     */
 
     public AgendaTrabajo() {
         contactosTrabajo = new ArrayList<ContactoTrabajo>();
     }
-
+/**
+     * Método que devuelve la lista de contacto de trabajo almacenado en la agenda.
+     * @return la lista de contactos de trabajo almacenados en la agenda.
+     */
     public ArrayList<ContactoTrabajo> getContactosTrabajo() {
         return contactosTrabajo;
     }
-
+/**
+     * Método que establece la lista de contacto de trabajo almacenados en la agenda.
+     * @param contactosTrabajo la nueva lista de contactos de trabajo a almacenar en la agenda.
+     */
     public void setContactosTrabajo(ArrayList<ContactoTrabajo> contactosTrabajo) {
         this.contactosTrabajo = contactosTrabajo;
     }
     
-
+ /**
+     * Método toString devuelve una cadena de texto que representa la agenda de trabajo.
+     * @return Cadena de texto que representa la agenda de trabajo.
+     */
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
